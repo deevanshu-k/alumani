74c5fa026a36207ef72c34776c0aa6bd7f3caece
Deevanshu Kushwah <astroboiscosmos@gmail.com>
2022-09-07 18:37:20 +0530

    coackroachdb connected + jasmin-module-added

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 const {Sequelize} = require("sequelize");
 
-const sequelize = new Sequelize(
-   process.env.db_name,
-   process.env.db_username,
-   process.env.db_password,
-    {
-      host: process.env.db_host,
-      dialect: process.env.db_dialect
-    }
-  );
-
+// const sequelize = new Sequelize(
+//    process.env.db_name,
+//    process.env.db_username,
+//    process.env.db_password,
+//     {
+//       host: process.env.db_host,
+//       dialect: process.env.db_dialect
+//     }
+//   );
+const sequelize = new Sequelize(process.env.DATABASE_URL);
   
 sequelize.authenticate().then(() => {
     console.log('Connection has been established successfully.');
