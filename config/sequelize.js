fd80f744d8213ba6766ce0f5f720f40ea385a873
Deevanshu <deevanshukushwah80@gmail.com>
2023-02-13 21:13:07 +0530

    sequelize.js updated for dev/prod

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,21 +1,25 @@
 require('dotenv').config();
 const {Sequelize} = require("sequelize");
+let sequelize;
 
-// const sequelize = new Sequelize(
-//    process.env.db_name,
-//    process.env.db_username,
-//    process.env.db_password,
-//     {
-//       host: process.env.db_host,
-//       dialect: process.env.db_dialect
-//     }
-//   );
-const sequelize = new Sequelize(process.env.DATABASE_URL,{
-   dialect: 'postgres',
-   dialectOptions: {
-       ssl: true
-   }
-});
+if(process.env.NODE_ENV == 'production'){
+   sequelize = new Sequelize(
+      process.env.db_name,
+      process.env.db_username,
+      process.env.db_password,
+       {
+         host: process.env.db_host,
+         dialect: process.env.db_dialect
+       }
+     );
+}else {
+   sequelize = new Sequelize(process.env.DATABASE_URL,{
+      dialect: 'postgres',
+      dialectOptions: {
+          ssl: true
+      }
+   });
+}
   
 sequelize.authenticate().then(() => {
     console.log('Connection has been established successfully.');
