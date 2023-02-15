f409d9cb2b7d3678ba711ba2feef9322e33899e7
Deevanshu <deevanshukushwah80@gmail.com>
2023-02-15 12:58:43 +0530

    submitSpinner added + imageSize 50kb updated + copyToClipboard( ) updated

--- static/js/index.js
+++ static/js/index.js
@@ -3,7 +3,7 @@ let uId = localStorage.getItem('uniqueId');
 let submitted = document.getElementById('submitted');
 let uniqueIdDisplay = document.getElementById('uniqueIdDisplay');
 uniqueIdDisplay.innerText = uId || '0000XX0000';
-submitted.style.display = 'none';
+// submitted.style.display = 'none';
 var profilePhoto = document.getElementById('profilePhoto');
 var profilePhotoPreview = document.getElementById('profilePhotoPreview');
 var pout_year = document.getElementById("pout_year");
@@ -11,6 +11,7 @@ var adm_year = document.getElementById("adm_year");
 var alert = document.getElementById('alert');
 var form = document.querySelector('#alumani-form');
 let submitbtn = document.getElementById('submitbtn');
+let submitbtnspinner = document.getElementById('submitbtnspinner');
 
 pout_year.max = (new Date()).getFullYear();
 adm_year.max = (new Date()).getFullYear();
@@ -19,8 +20,13 @@ if (state == 'submitted') {
     form.style.display = "none";
     submitted.style.display = '';
 }
+else {
+    form.style.display = "";
+    submitted.style.display = 'none';
+}
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
+    submitbtnspinner.style.display = '';
     submitbtn.setAttribute('disabled', true);
     let uniqueId = form.adm_year.value + form.email.value;
     let data = {
@@ -50,6 +56,7 @@ form.addEventListener('submit', async (e) => {
             form.style.display = "none";
             uniqueIdDisplay.innerText = d.data.uniqueId;
             submitted.style.display = '';
+            submitbtnspinner.style.display = 'none';
             form.reset();
         }
         else if (img.status != 200) {
@@ -59,9 +66,11 @@ form.addEventListener('submit', async (e) => {
             form.style.display = "none";
             uniqueIdDisplay.innerText = d.data.uniqueId;
             submitted.style.display = '';
+            submitbtnspinner.style.display = 'none';
             form.reset();
         } else {
             alertBox("Server error, try again after some time");
+            submitbtnspinner.style.display = 'none';
             form.reset();
         }
     });
@@ -80,7 +89,7 @@ async function uploadImage(Id) {
     return img;
 }
 
-async function submitForm(data){
+async function submitForm(data) {
     let res = await fetch('/alumni', {
         method: 'POST',
         body: JSON.stringify(data),
@@ -93,25 +102,30 @@ async function submitProfilePhoto(event) {
     profilePhotoPreview.src = URL.createObjectURL(event.target.files[0]);
     let fileSize = event.target.files[0].size;
     let fileMb = fileSize / 1024 ** 2;
-    if (fileMb > 0.5) {
+    if (fileMb > 0.05) {
         profilePhotoPreview.src = "../static/default_profile_photo.webp";
-        alertBox("select the image with a size up to 500Kb");
+        alertBox("select the image with a size up to 50Kb");
     }
 }
 
 function anotherResponse() {
     localStorage.removeItem('state');
     form.style.display = "";
+    submitbtnspinner.style.display = 'none';
     uniqueIdDisplay.innerText = '0000XX0000';
-    localStorage.setItem('uniqueId','0000XX0000')
+    localStorage.setItem('uniqueId', '0000XX0000')
     submitted.style.display = 'none';
     document.getElementById('submitbtn').removeAttribute('disabled');
 }
 
 function copyToClipboard() {
     let text = uniqueIdDisplay.innerText;
-    navigator.clipboard.writeText(text);
-    alertBox("Copied the text: " + text);
+    if (window.isSecureContext && navigator.clipboard) {
+        navigator.clipboard.writeText(text);
+        alertBox("Copied the text: " + text);
+    } else {
+        alertBox("Unable to copy to clipboard");
+    }
 }
 
 function alertBox(msg) {
